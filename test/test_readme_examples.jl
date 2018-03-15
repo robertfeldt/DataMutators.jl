6cb928b3e120869f5432ae783540095718020f06
Robert Feldt <robert.feldt@gmail.com>
2018-03-15 16:01:25 +0100

    added also 3rd example from README

--- test/test_readme_examples.jl
+++ test/test_readme_examples.jl
@@ -13,8 +13,18 @@ end
 @testset "Example 2: buggyreverse shrinking array of strings" begin
     fd2 = ["longer", "strings", "than", "needed"]
     res2 = shrink(fd2, prop_last_is_first)
-    @test length(res2) == 2
+    @test length(res2) == 2 
     @test sort(map(length, res2)) == [0, 1]
+end
+
+@testset "Example 3: buggyreverse shrinking array of data of multiple types" begin
+    fd3 = [0, "than", "needed", [1.1, 2, "ab"]]
+    res3 = shrink(fd3, prop_last_is_first)
+    @test length(res3) == 2
+    lens = sort(map(length, res3))
+    @show res3
+    # One short and one slightly longer, since not always gets down to minimal
+    @test lens[1] < lens[2] < 5
 end
 
 end
