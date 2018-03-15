904de4861e6e7db6deb03c56830f376be14c96a6
Robert Feldt <robert.feldt@gmail.com>
2018-03-15 17:26:08 +0100

    simple search to find other switches

--- spikes/finding_exception_in_date_parsing.jl
+++ spikes/finding_exception_in_date_parsing.jl
@@ -26,3 +26,34 @@ datum, lenreduction, count, str, dtr, swtr = shrink_until(valid_starting_point, 
 # This is probably why we don't see switches on the borders we know are there such as
 # 2018-03-32 etc. We need to use DataMutators better with search to find these, other 
 # borders.
+
+# Let's try with a simple search that just repeats each mutator we give it in sequence
+# to try to find switches.
+function property_switching_search(datum, mutators, property::Function; maxrepsPerMutator = 1000)
+    orig_prop_value = property(datum)
+
+    origdatum = deepcopy(datum)
+    results = Any[]
+
+    for m in mutators
+        datum = deepcopy(origdatum) 
+        for i in 1:maxrepsPerMutator
+            muts = DataMutators.mutations(m, datum)
+            newdatum = DataMutators.apply(muts, datum)
+            if property(newdatum) != property(datum)
+                push!(results, (datum, newdatum, muts))
+            end
+            datum = newdatum
+        end
+    end
+
+    return results
+end
+
+res1 = property_switching_search(valid_starting_point, [DataMutators.IncreaseIntKeepingSize], canparsedt; maxrepsPerMutator = 1000)
+
+res2 = property_switching_search(valid_starting_point, [DataMutators.IncreaseInt], canparsedt; maxrepsPerMutator = 100)
+
+res3 = property_switching_search(valid_starting_point, [DataMutators.DecreaseIntKeepingSize], canparsedt; maxrepsPerMutator = 1000)
+
+res4 = property_switching_search(valid_starting_point, [DataMutators.DecreaseInt], canparsedt; maxrepsPerMutator = 100)
